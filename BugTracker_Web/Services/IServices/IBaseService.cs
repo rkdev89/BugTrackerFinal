434e15d87647b0fed3ae8ce4c6c410d37c094075
rkdev89 <richardkemp35@gmail.com>
2023-02-02 21:45:25 +0000

    adding base service for API

---
+++ BugTracker_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using BugTracker_Web.Models;
+
+namespace BugTracker_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+    }
+}
