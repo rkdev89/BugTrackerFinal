54da0aa18eb10295472410b30e2b3e5cb7970396
rkdev89 <richardkemp35@gmail.com>
2023-02-02 17:48:26 +0000

    add automapper

---
+++ BugTracker_API/MappingConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BugTracker_API.Models;
+using BugTracker_API.Models.Dto;
+
+namespace BugTracker_API
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Bug, BugDTO>();
+            CreateMap<BugDTO, Bug>();
+
+            CreateMap<User, UserDTO>();
+            CreateMap<UserDTO, User>();
+
+            CreateMap<Bug, BugCreateDTO>().ReverseMap();
+            CreateMap<User, UserCreateDTO>().ReverseMap();
+
+            CreateMap<Bug, BugUpdateDTO>().ReverseMap();
+            CreateMap<User, UserUpdateDTO>().ReverseMap();
+        }
+    }
+}
--- BugTracker_API/Program.cs
+++ BugTracker_API/Program.cs
@@ -1,3 +1,4 @@
+using BugTracker_API;
 using BugTracker_API.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
 });
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddControllers().AddNewtonsoftJson().AddXmlSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
